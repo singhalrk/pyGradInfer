34de9c5ae50074bf642cdba008e03719ad68e865
Raghav Singhal <singhal.raghav@gmail.com>
2018-10-22 15:06:50 -0400

    added fgpgm file

---
+++ src/fgpgm.py
@@ -0,0 +1,11 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+from kernels.rbf import RBF
+
+
+class FGPGM:
+    def __init__(self,):
+        raise NotImplementedError
