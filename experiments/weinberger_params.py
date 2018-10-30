5701c6964064a48a7be76c4909b06200ca6a00b8
singhalrk <singhal.raghav@gmail.com>
2018-10-29 22:07:07 -0400

    added ode solvers and experiments

---
+++ experiments/weinberger_params.py
@@ -0,0 +1,37 @@
+from collections import namedtuple
+import numpy as np
+
+
+Parameters = namedtuple('Parameters', 'b_max, K, k, d, delta, p, c, q_max')
+
+d = 0.02  # uninfected-cell death rate
+delta = 0.7  # infected-cell death rate
+c = 30   # virion decay rate
+k = 1.875e-7  # infectivity rate
+
+n = 200
+p = n*delta  # virion production rate
+
+U0 = 8e+5
+I0 = 0
+V0 = 1e+6
+s0 = 8e+5 * 10
+
+# TODO change these to dimensional
+b_max = 40
+q_max = 25
+K = 2e+6
+
+X0 = np.array([U0, I0, V0, s0])
+
+Weinberger_Params = Parameters(d=d,
+                               K=K,
+                               k=k,
+                               q_max=q_max,
+                               b_max=b_max,
+                               p=p,
+                               c=c,
+                               delta=delta)
+
+
+# fix feed
--- src/ode_solvers.py
+++ src/ode_solvers.py
@@ -1,9 +1,73 @@
 import numpy as np
 
 
-def rk4():
-    pass
+def euler(x, f, eps, params=None, time=None):
+    if params is None:
+        return x + eps * f(x)
+
+    elif params is not None and time is not None:
+        return x + eps * f(x, params, time)
+
+    return x + eps * f(x, params)
 
 
-def euler():
-    pass
+def rk2(x, f, eps, params=None, time=None):
+    """RK2 Heun"""
+    if params is None:
+        k1 = f(x)
+        k2 = f(x + eps*k1)
+        return x + (k1 + k2)*(eps / 2)
+
+    elif params is not None and time is not None:
+        k1 = f(x, params, time)
+        k2 = f(x + eps*k1, params, time)
+
+        return x + eps * f(x, params, time)
+
+    k1 = f(x)
+    k2 = f(x + eps*k1)
+    return x + (k1 + k2)*(eps / 2)
+
+
+def rk4(x, f, eps, params=None, time=None):
+    if params is None:
+        k1 = f(x)
+        k2 = f(x + 0.5 * eps * k1)
+        k3 = f(x + 0.5 * eps * k2)
+        k4 = f(x + eps * k3)
+
+    elif params is not None and time is None:
+        k1 = f(x, params)
+        k2 = f(x + 0.5 * eps * k1, params)
+        k3 = f(x + 0.5 * eps * k2, params)
+        k4 = f(x + eps * k3, params)
+
+    elif params is not None and time is not None:
+        k1 = f(x, params, time)
+        k2 = f(x + 0.5 * eps * k1, params, time)
+        k3 = f(x + 0.5 * eps * k2, params, time)
+        k4 = f(x + eps * k3, params, time)
+
+    return x + (eps / 6) * (k1 + 2*k2 + 2*k3 + k4)
+
+
+def solve(x0, F, T, eps=1e-3, method_name='rk4', params=None):
+    if method_name == 'rk4':
+        method = rk4
+    elif method_name == 'euler':
+        method = euler
+
+    x0 = np.asarray(x0)
+    path = [x0]
+
+    N = int(T / eps)            # time steps
+
+    for i in range(N - 1):
+        if params is None:
+            x0 = method(x0, F, eps)
+        else:
+            x0 = method(x0, F, eps, params)
+
+        path.append(x0)
+
+    return np.array(path)
