490739aab783f8344d144d2a017c585dea2d9919
singhalrk <singhal.raghav@gmail.com>
2018-10-29 22:41:51 -0400

    removed time from ode solvers, added potential sections to main.tex

--- src/ode_solvers.py
+++ src/ode_solvers.py
@@ -1,52 +1,23 @@
 import numpy as np
 
 
-def euler(x, f, eps, params=None, time=None):
-    if params is None:
-        return x + eps * f(x)
-
-    elif params is not None and time is not None:
-        return x + eps * f(x, params, time)
-
+def euler(x, f, eps, params=None):
     return x + eps * f(x, params)
 
 
-def rk2(x, f, eps, params=None, time=None):
+def rk2(x, f, eps, params=None):
     """RK2 Heun"""
-    if params is None:
-        k1 = f(x)
-        k2 = f(x + eps*k1)
-        return x + (k1 + k2)*(eps / 2)
 
-    elif params is not None and time is not None:
-        k1 = f(x, params, time)
-        k2 = f(x + eps*k1, params, time)
-
-        return x + eps * f(x, params, time)
-
-    k1 = f(x)
-    k2 = f(x + eps*k1)
+    k1 = f(x, params)
+    k2 = f(x + eps*k1, params)
     return x + (k1 + k2)*(eps / 2)
 
 
-def rk4(x, f, eps, params=None, time=None):
-    if params is None:
-        k1 = f(x)
-        k2 = f(x + 0.5 * eps * k1)
-        k3 = f(x + 0.5 * eps * k2)
-        k4 = f(x + eps * k3)
-
-    elif params is not None and time is None:
-        k1 = f(x, params)
-        k2 = f(x + 0.5 * eps * k1, params)
-        k3 = f(x + 0.5 * eps * k2, params)
-        k4 = f(x + eps * k3, params)
-
-    elif params is not None and time is not None:
-        k1 = f(x, params, time)
-        k2 = f(x + 0.5 * eps * k1, params, time)
-        k3 = f(x + 0.5 * eps * k2, params, time)
-        k4 = f(x + eps * k3, params, time)
+def rk4(x, f, eps, params=None):
+    k1 = f(x, params)
+    k2 = f(x + 0.5 * eps * k1, params)
+    k3 = f(x + 0.5 * eps * k2, params)
+    k4 = f(x + eps * k3, params)
 
     return x + (eps / 6) * (k1 + 2*k2 + 2*k3 + k4)
 
